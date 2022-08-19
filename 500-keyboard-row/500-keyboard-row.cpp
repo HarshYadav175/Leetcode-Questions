7e7f996ba519d15847402bf67a6a8a03b17423bb
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2022-08-20 00:48:45 +0530

    Time: 4 ms (32.48%), Space: 6.8 MB (86.86%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    unordered_map<char, int> m;
+    
+    bool check(string s,char c)
+    {
+        int row = m[c];
+        
+        for(int i = 0 ; i < s.size(); i++)
+        {
+            char ch = tolower(s[i]);
+            if(m[ch] != row) return false;
+        }
+        return true;
+    }
+    vector<string> findWords(vector<string>& words) {
+        string s1 = "qwertyuiop";
+        string s2 = "asdfghjkl";
+        string s3 = "zxcvbnm";
+        for(char i : s1)
+        {
+            m[i] = 1;
+        }
+        for(char i : s2)
+        {
+            m[i] = 2;
+        }
+        for(char i : s3)
+        {
+            m[i] = 3;
+        }
+        
+        vector<string> v;
+        for(int i = 0; i < words.size(); i++)
+        {
+            string s = words[i];
+            char ch = tolower(s[0]);
+            if(check(s, ch))
+            {
+                v.push_back(s);
+            }
+        }
+        
+        return v;
+    }
+};
