be7ee95a67bb8cbe2149333ba888a95a5d6d34ca
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2022-08-23 22:22:24 +0530

    Time: 46 ms (99.94%), Space: 21.5 MB (99.93%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -9,13 +9,14 @@ public:
         
         nums.push_back(0);
         for ( int i = 0; i <= n; i++ ) {
-            while ( nums[nums[i]] != nums[i] ) {
+            while ( nums[nums[i]] != nums[i] ){
                 swap ( nums[i], nums[nums[i]] );
             }
         }
         
-        for ( int i = 1; i <= n; i++ ) {
-            if ( i != nums[i] ) return i;
+        for ( int i = 1; i <= n; i++ ){
+            if ( i != nums[i] ) 
+                return i;
         }
         
         return n+1;
