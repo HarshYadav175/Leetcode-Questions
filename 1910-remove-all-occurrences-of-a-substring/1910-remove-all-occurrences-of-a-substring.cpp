6c6dc7a3d7e533ef8207b32c45c957b8c7636e6b
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2022-08-25 18:19:22 +0530

    Time: 4 ms (76.29%), Space: 6.7 MB (21.25%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        
+        int length = part.length();
+        
+        // find will be used to find a subarray in the string.
        while(s.length()!=0 && s.find(part)<s.length()){
-        s.erase(s.find(part), part.length());
+        s.erase(s.find(part), length);
        }
         
         return s;
