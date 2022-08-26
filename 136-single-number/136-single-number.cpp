4946a94d857e21e7111648edf6e2ec60c92b871f
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2022-08-26 20:01:52 +0530

    Time: 30 ms (60.80%), Space: 16.9 MB (86.59%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        int n = nums.size();
         
-        for(auto i:nums)
-              ans^= i;
+        int n = nums.size();  // extracting the size of the array
         
-        return ans;
+        // traverse from the array
+        for(int i = 0; i < n-1 ; i++)
+        {
+            nums[i+1] = nums[i] ^ nums[i + 1]; // (prev answer xor current index)
         }
+        
+        return nums[n-1]; // return left over element
+    }
 };
