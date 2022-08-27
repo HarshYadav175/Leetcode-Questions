8f33f68d398631419b1f3634d1fa7e39a1622a6a
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2022-08-27 13:53:37 +0530

    Time: 3 ms (64.11%), Space: 8.4 MB (23.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
+        vector<int> ans;
+        vector<int> ans1;
+        vector<int> ans2;
+        
+        int n=nums.size();
+       
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                ans.push_back(0);
+                
+            }
+            else  if(nums[i]==1)
+            {
+                ans1.push_back(1);
+                
+            }
+            else
+                 {
+                ans2.push_back(2);
+                
+            }
+            
+    
+    }
+        nums.clear();
+            for(int i=0;i<ans.size();i++)
+            {
+                nums.push_back(ans[i]);
+            }
+        for(int i=0;i<ans1.size();i++)
+            {
+                nums.push_back(ans1[i]);
+            }
+        for(int i=0;i<ans2.size();i++)
+            {
+                nums.push_back(ans2[i]);
+            }
     }
 };
