fe1c8b8084d45c09742dd8a1a87c68c82425d8d0
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2023-06-19 21:50:25 +0530

    Time: 11 ms (73.50%), Space: 8.2 MB (36.39%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,8 +12,6 @@ public:
        ListNode * slow=head;
        ListNode * fast=head;
         
-        
-        
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
