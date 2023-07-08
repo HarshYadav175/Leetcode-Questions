a83ae5f411445ae1a9cd3fe340ce8293f1950913
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2023-07-09 01:14:02 +0530

    Time: 16 ms (29.36%), Space: 5.9 MB (93.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        int ans;
         
-        if(n<=1)
-            
+        if(n<=1){
             return n;
-          
-        return ans=fib(n-1)+fib(n-2);
+        }
+        
+        return fib(n-1) + fib(n-2);
+        
     }
 };
