26eec055327ed7ea97720e4b8d58a6291598ea19
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2023-08-20 14:07:10 +0530

    Time: 30 ms (55.85%), Space: 25 MB (51.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-       k = k % n; // To handle cases where k is larger than n
         
-        reverse(nums.begin(), nums.end()); // Reverse the entire vector
-        reverse(nums.begin(), nums.begin() + k); // Reverse the first k elements
-        reverse(nums.begin() + k, nums.end()); // Reverse the remaining elements after the first k
+        k = k % n;
+        
+        
+
+        reverse(nums.begin(), nums.end());
+        
+       
+        reverse(nums.begin(), nums.begin() + k);
+        
+        
+        reverse(nums.begin() + k, nums.end());
     }
 };
+
