8fb6b4c5b9c5e526cf849c2e3b134be632696851
Harsh Yadav <106460778+HarshYadav175@users.noreply.github.com>
2023-08-26 15:38:14 +0530

    Time: 171 ms (85.71%), Space: 114.3 MB (69.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,44 +11,43 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-              // edge cases
-        if(head==NULL || head->next==NULL) return true;
         
-        //Step 1: Find middle of linked list
-        ListNode* slow=head;
-        ListNode* fast=head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* start = head;
         
-        while(fast && fast->next && fast->next->next) {
-            slow=slow->next;
-            fast=fast->next->next;
+        while(fast != NULL && fast->next != NULL){
+             slow = slow->next;
+            fast = fast->next->next;
         }
         
+        // Reverse the second half
+        slow = reverseList(slow);
         
-        //Step 2: Reverse second half of linked list
-    slow->next=reverse(slow->next);
-    slow=slow->next;
+        while(slow != NULL){
+            if(start->val != slow->val){
+                return false;
+            }
+            start = start->next;
+            slow = slow->next;
+        }
         
-        //Step 3: Compare
-while(slow!=NULL){
-    if(head->val!=slow->val)return false;
-    head=head->next;
-    slow=slow->next;
-}
         return true;
-}   
-    // function to reverse linked list
-ListNode* reverse(ListNode* head){
-    ListNode* current=head;
-    ListNode* nextptr=NULL;
-    ListNode*previous=NULL;
-    
-    while(current!=NULL){
-        nextptr=current->next;
-        current->next=previous;
-        
-        previous=current;
-        current=nextptr;
     }
-    return previous;
-}
+
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* previous = NULL;
+        ListNode* current = head;
+        ListNode* nextptr = NULL;
+        
+        while(current != NULL){
+            nextptr = current->next;
+            current->next = previous;
+            previous = current;
+            current = nextptr;
+        }
+            
+        return previous;
+    }
 };
